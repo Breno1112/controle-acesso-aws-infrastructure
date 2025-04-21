01e2a95e2b2f7f0da0b5579b91840c5907b356c4
Breno1112 <contato.brenofachini@hotmail.com>
2025-04-21 04:54:04 -0300

    going to try to build and send lambda function

--- apps/unlock-lock/handler.py
+++ apps/unlock-lock/handler.py
@@ -1,14 +1,32 @@
 import json
+import boto3
 
 def lambda_handler(event, context):
+    iot_client = boto3.client('iot-data', region_name='sa-east-1')
     print("Received event:", json.dumps(event))
-
-    return {
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "body": json.dumps({
-            "success": True
-        })
-    }
+    try:
+        iot_client.publish(
+            topic='controleacesso-nodemcu/sub'
+            qos=1,
+            payload='D'
+        )
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": json.dumps({
+                "success": True
+            })
+        }
+    except:
+        return {
+            "statusCode": 412,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": json.dumps({
+                "success": False,
+                "errorReason": "SYSTEM_OFFLINE"
+            })
+        }
